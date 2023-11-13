ca5d6ee5859f7f0a78ea101dfae5c925c9e7a8cf
Owen Rees <owenrees82@gmail.com>
2023-11-13 20:51:41 +0100

    handleInputChange refactor

--- src/app/chessboard/FormField.tsx
+++ src/app/chessboard/FormField.tsx
@@ -1,16 +1,9 @@
-import { FormFieldProps, Header } from "@/types";
-import { ChangeEvent } from "react";
+import { FormFieldProps } from "@/types";
 import { useGameContext } from "@/context/GameContext";
+import { handleInputChange } from "@/handlers/contactHandlers";
 
 const FormField = ({ fieldName, type } : FormFieldProps) => {
   const { gameState, gameDispatch } = useGameContext()
-
-  // TODO move to handlers
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault()
-    const updatedHeaders = { ...gameState.headers, [fieldName]: e.target.value } as Header;
-    gameDispatch({ type: 'SET_HEADERS', payload: updatedHeaders })
-  };
 
   return (
     <div className="flex flex-col w-full text-left">
@@ -18,7 +11,7 @@ const FormField = ({ fieldName, type } : FormFieldProps) => {
       <input className="block p-2.5 border border-gray-500 text-gray-900 text-sm rounded-lg focus:border-primary-500 focus:ring-primary-500 shadow-sm"
              type={type}
              id={fieldName}
-             value={gameState.headers[fieldName]} onChange={handleInputChange} />
+             value={gameState.headers[fieldName]} onChange={(e) => handleInputChange(e, gameState, gameDispatch, fieldName)} />
     </div>
   )
 }
--- src/handlers/contactHandlers.ts
+++ src/handlers/contactHandlers.ts
@@ -1,6 +1,17 @@
-import { Dispatch, SetStateAction } from "react";
-import { ContactFormValues, MessageStatus } from "@/types";
+import { Dispatch, SetStateAction, ChangeEvent } from "react";
+import { ContactFormValues, GameProps, MessageStatus, Header } from "@/types";
 import { type UseFormReset } from "react-hook-form";
+
+export const handleInputChange = (
+  e: ChangeEvent<HTMLInputElement>,
+  gameState: GameProps,
+  gameDispatch: any, // TODO fix any
+  fieldName: string,
+) => {
+  e.preventDefault();
+  const updatedHeaders = { ...gameState.headers, [fieldName]: e.target.value } as Header;
+  gameDispatch({ type: 'SET_HEADERS', payload: updatedHeaders })
+};
 
 export const handleContactSubmit = async (
   data: ContactFormValues,
@@ -9,37 +20,37 @@ export const handleContactSubmit = async (
   setMessageStatus: Dispatch<SetStateAction<MessageStatus>>
 ) => {
 
-    try {
+  try {
+    setMessageStatus(prevState => ({
+      ...prevState,
+      isSending: true
+    }));
+
+    reset();  // clear form fields
+
+    const response = await contact.mutateAsync(data);
+    if (response) {
+      setMessageStatus(prevState => ({
+        isSuccess: true,
+        isSending: false,
+        message: "Message sent!"
+      }));
+
+    }
+  } catch (error) {
+    console.log(error);
+    setMessageStatus(prevState => ({
+      isSuccess: false,
+      isSending: false,
+      message: "Something went wrong. Please try again later."
+    }));
+  } finally {
+    setTimeout(() => {
       setMessageStatus(prevState => ({
         ...prevState,
-        isSending: true
-      }));
-
-      reset();  // clear form fields
-
-      const response = await contact.mutateAsync(data);
-      if (response) {
-        setMessageStatus(prevState => ({
-          isSuccess: true,
-          isSending: false,
-          message: "Message sent!"
-        }));
-
-      }
-    } catch (error) {
-      console.log(error);
-      setMessageStatus(prevState => ({
         isSuccess: false,
-        isSending: false,
-        message: "Something went wrong. Please try again later."
+        message: ""
       }));
-    } finally {
-      setTimeout(() => {
-        setMessageStatus(prevState => ({
-          ...prevState,
-          isSuccess: false,
-          message: ""
-        }));
-      }, 5000);
-    }
-}
+    }, 5000);
+  }
+};
