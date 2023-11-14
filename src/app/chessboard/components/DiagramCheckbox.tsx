fe857113a41cefd8cfbf98faa308a34e08bcf519
Owen Rees <owenrees82@gmail.com>
2023-11-14 16:16:16 +0100

    chessboard seperation of CSR

--- src/app/chessboard/components/DiagramCheckbox.tsx
+++ src/app/chessboard/components/DiagramCheckbox.tsx
@@ -1,10 +1,10 @@
-'use client'
+"use client";
 
 import { GameProps } from "@/types";
 import { MutableRefObject } from "react";
 import { diagramCheckboxHandler } from "@/handlers/diagramCheckboxHandlers";
 
-const DiagramCheckbox = ({ checkboxRef, gameState, gameDispatch } : {
+const DiagramCheckbox = ({ checkboxRef, gameState, gameDispatch }: {
   checkboxRef: MutableRefObject<HTMLInputElement | null>,
   gameState: GameProps,
   gameDispatch: any
@@ -12,19 +12,19 @@ const DiagramCheckbox = ({ checkboxRef, gameState, gameDispatch } : {
 
   return (
     <div className="flex justify-start mt-4">
-        <label htmlFor="diagramCheckbox">Select Diagram</label>
-        <input type="checkbox"
-               className="ml-2 accent-primary-200"
-               id="diagramCheckbox"
-               disabled
-               ref={checkboxRef}
-               onChange={() => diagramCheckboxHandler(
-                   gameState,
-                   gameDispatch,
-                   checkboxRef)}
-        />
+      <label htmlFor="diagramCheckbox">Select Diagram</label>
+      <input type="checkbox"
+             className="ml-2 accent-primary-200"
+             id="diagramCheckbox"
+             disabled
+             ref={checkboxRef}
+             onChange={() => diagramCheckboxHandler(
+               gameState,
+               gameDispatch,
+               checkboxRef)}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default DiagramCheckbox
+export default DiagramCheckbox;
--- src/app/chessboard/page.tsx
+++ src/app/chessboard/page.tsx
@@ -1,83 +1,15 @@
-'use client' // TODO can we move this to the form/fields?
-
 import type { Metadata } from "next";
+import ChessboardLayout from "@/app/chessboard/ChessboardLayout";
 
-import dynamic from "next/dynamic";
-import { useMemo, useRef, useState } from "react";
-
-import SectionLargeHeading from "@/app/components/SectionLargeHeading";
-import HeaderFields from "@/app/chessboard/components/HeaderFields";
-import GameLoadButtons from "@/app/chessboard/components/GameLoadButtons";
-import LoadingBoard from "@/app/chessboard/components/LoadingBoard";
-import Alert from "@/app/components/Alert";
-import DiagramCheckbox from "@/app/chessboard/components/DiagramCheckbox";
-
-import { useGameContext } from "@/context/GameContext";
-import { diagramCheckboxHandler } from "@/handlers/diagramCheckboxHandlers";
-import useBoardButtonClicks from "@/hooks/useBoardButtonClicks";
-import useMoveListClicks from "@/hooks/useMoveListClicks";
-import GameSaveButtons from "@/app/chessboard/components/GameSaveButtons";
-
-// TODO metadata not allowed inside a client component, which is why 'use client' needs moving
-// export const metadata: Metadata = {
-//     title: 'Chess PDF - Chessboard',
-//     description: 'Convert your PGN file to a PDF of your chess game'
-// }
-
-const GameBoard = dynamic(() => import("./components/Board"), {
-    ssr: false,
-    loading: LoadingBoard
-})
+export const metadata: Metadata = {
+    title: 'Chess PDF - Chessboard',
+    description: 'Convert your PGN file to a PDF of your chess game'
+}
 
 const Chessboard = () => {
-    const { gameState, gameDispatch } = useGameContext()
-    const { diagrams, pgn} = gameState
-    const checkboxRef = useRef<HTMLInputElement>(null)
-    const [message, setMessage] = useState({
-        type: '',
-        message: ''
-    })
-
-    useBoardButtonClicks(checkboxRef)
-    useMoveListClicks(checkboxRef)
-
-    // TODO move into its own component
-    // const diagramCheckbox = () => {
-    //     return (
-    //         <div className="flex justify-start mt-4">
-    //             <label htmlFor="diagramCheckbox">Select Diagram</label>
-    //             <input type="checkbox"
-    //                    className="ml-2 accent-primary-200"
-    //                    id="diagramCheckbox"
-    //                    disabled
-    //                    ref={checkboxRef}
-    //                    onChange={() => diagramCheckboxHandler(
-    //                        gameState,
-    //                        gameDispatch,
-    //                        checkboxRef)}
-    //             />
-    //         </div>
-    //     )
-    // }
-
-    // memoized chessboard that only re-renders with gamePGN state change
-    const chessBoardMemoized = useMemo(() => {
-        return <GameBoard diagrams={diagrams}>{pgn}</GameBoard>
-    }, [pgn])
-
     return (
         <>
-            <main className="grid max-w-screen-2xl items-center px-8">
-                <SectionLargeHeading text="Convert PGN to" textAccent="PDF"/>
-                <GameLoadButtons/>
-                <div className="flex justify-center">
-                    {chessBoardMemoized}
-                </div>
-                <DiagramCheckbox checkboxRef={checkboxRef} gameState={gameState} gameDispatch={gameDispatch} />
-                <GameSaveButtons setMessage={setMessage}/>
-                <Alert type={message.type} message={message.message} setMessage={setMessage} />
-                <HeaderFields/>
-            </main>
+            <ChessboardLayout />
         </>
     )
 }
