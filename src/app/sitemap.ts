b56c6d7c2db4b02b70778efff5bde96bd6ca6052
Owen Rees <owenrees82@gmail.com>
2023-11-14 11:20:12 +0100

    sitemap added

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,1 +1,24 @@
-// TODO sitemap
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://chess-pdf.vercel.app/',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://chess-pdf.vercel.app/chessboard',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://chess-pdf.vercel.app/contact',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    }
+  ]
+}
