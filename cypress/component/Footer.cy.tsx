3c088604697ecf7c9f2125495b49bb06ee933ae9
Owen Rees <owenrees82@gmail.com>
2023-11-15 22:06:51 +0100

    Footer component tests

---
+++ cypress/component/Footer.cy.tsx
@@ -0,0 +1,31 @@
+import Footer from '@/app/components/Footer'
+
+describe('<Footer />', () => {
+  beforeEach(() => {
+    cy.mount(<Footer />)
+  })
+
+  it('mounts', () => {
+    cy.get('footer').should('be.visible')
+  })
+
+  it('has a link to the homepage', () => {
+    cy.get('[data-cy="homepage-link"]').should('be.visible')
+    cy.get('[data-cy="homepage-link"]').should('have.attr', 'href', '/')
+  })
+
+  it('has a link to the github repo', () => {
+    cy.get('[data-cy="github-link"]').should('be.visible')
+    cy.get('[data-cy="github-link"]').should('have.attr', 'href', Cypress.env('GITHUB_URL'))
+  })
+
+  it('has a link to the contact page', () => {
+    cy.get('[data-cy="contact-link"]').should('be.visible')
+    cy.get('[data-cy="contact-link"]').should('have.attr', 'href', '/contact')
+  })
+
+  it('has a copyright notice with the current year', () => {
+    cy.get('[data-cy="copyright"]').should('be.visible')
+    cy.get('[data-cy="copyright"]').should('contain', new Date().getFullYear())
+  })
+})
