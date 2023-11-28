8e0cc6f7231e07c312e1773c88055a8367678524
Owen Rees <owenrees82@gmail.com>
2023-11-28 22:42:25 +0100

    move list clicks

--- src/hooks/useBoardButtonClicks.ts
+++ src/hooks/useBoardButtonClicks.ts
@@ -6,7 +6,7 @@ import { useGameContext } from "@/context/GameContext";
 
 export const useLpvBoardButtonClicks = (checkboxRef: RefObject<HTMLInputElement>, lpvRef: RefObject<any>) => { // TODO fix any
   const { gameState, gameDispatch } = useGameContext()
-  const { diagrams  } = gameState
+  const { diagrams } = gameState
 
   useEffect(() => {
     const boardButtons = document.querySelector(".lpv__controls")
@@ -17,7 +17,6 @@ export const useLpvBoardButtonClicks = (checkboxRef: RefObject<HTMLInputElement>
       let moves = [...document.querySelectorAll('move')].filter(m => m.parentNode?.querySelector('variation') && m.className !== 'empty')
       let ply = moves.findIndex(m => m.classList.contains('current')) + 1
       let fen = lpvRef.current?.curData().fen
-      console.log(ply, fen)
       lpvDiagramCheckboxDisabledHandler(checkboxRef, ply)
       lpvDiagramCheckboxCheckedHandler(diagrams, checkboxRef, ply)
     })
