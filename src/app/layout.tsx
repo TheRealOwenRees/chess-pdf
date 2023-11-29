20d1454a6e9f8c8128fbf124e9f2dfde49ef5b54
Owen Rees <owenrees82@gmail.com>
2023-11-29 14:21:54 +0100

    contact form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,13 +23,13 @@ export default function RootLayout({
   children: ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
     <head>
       <link rel="icon" href="/favicon.svg" sizes="any"/>
     </head>
     <Providers>
       <body className={inter.className}>
-        <div className="bg-white text-gray-900 grid">
+        <div className="bg-base-100 text-gray-900 grid">
           <Navbar />
             <div className="relative min-h-[calc(100vh-114px-76px)] bg-white place-self-center">{children}</div>
           <Footer />
