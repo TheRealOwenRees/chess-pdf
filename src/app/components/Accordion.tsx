8445ae0469d321be7b2493cc746df96fbe5c0158
Owen Rees <owenrees82@gmail.com>
2023-11-29 20:26:59 +0100

    custom light theme init

--- src/app/components/Accordion.tsx
+++ src/app/components/Accordion.tsx
@@ -32,37 +32,6 @@ const FAQAccordion = () => {
         </div>
       </div>
     </div>
-    // <Accordion
-    //   className={className}
-    //   itemClasses={itemClasses}
-    //   data-test="accordion"
-    // >
-    //   <AccordionItem
-    //     key="1"
-    //     aria-label="Is this service free?"
-    //     title="Is this service free?"
-    //     indicator={({ isOpen }) => (isOpen ? <FaMinus /> : <FaPlus />)}
-    //     data-test="accordion-item"
-    //   >
-    //     <p data-test="accordion-text">Yes. There are no current plans to charge for this service. If in the future there is a payment plan, it will come with a generous free tier.</p>
-    //   </AccordionItem>
-    //   <AccordionItem
-    //     key="2"
-    //     aria-label="Will you be adding more features?"
-    //     title="Will you be adding more features?"
-    //     indicator={({ isOpen }) => (isOpen ? <FaMinus /> : <FaPlus />)}
-    //   >
-    //     <p>We have a lot more features to implement in the near future. If you have a feature that you would like to see implemented, please feel free to send us a message via our contact form.</p>
-    //   </AccordionItem>
-    //   <AccordionItem
-    //     key="3"
-    //     aria-label="Do you hold any copyright on the produced materials?"
-    //     title="Do you hold any copyright on the produced materials?"
-    //     indicator={({ isOpen }) => (isOpen ? <FaMinus /> : <FaPlus />)}
-    //   >
-    //     <p>No. We will never implement any terms whereby we have any interest in the materials created with this service.</p>
-    //   </AccordionItem>
-    // </Accordion>
   );
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,26 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  daisyui: {
+     themes: [
+       {
+         customLightTheme: {
+           primary: '#4a00ff',
+           'primary-content': '#d1dbff',
+           secondary: '#ff00d3',
+           'secondary-content': '#fff8fd',
+           accent: '#00d7c0',
+           'accent-content': '#00110e',
+           'neutral': '#2b3440',
+           'neutral-content': '#d7dde4',
+           'base-100': '#ffffff',
+           'base-200': '#f2f2f2',
+           'base-300': '#e5e6e6',
+           'base-content': '#1f2937',
+         }
+       }
+     ]
+  },
   plugins: [
     require('@tailwindcss/typography'),
     require('daisyui')
