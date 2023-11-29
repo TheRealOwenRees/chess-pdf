18df1eb6f945fbef14a702780bbb455338c403a6
Owen Rees <owenrees82@gmail.com>
2023-11-29 20:04:58 +0100

    svg colours and accordion

--- src/app/components/SectionSmallHeading.tsx
+++ src/app/components/SectionSmallHeading.tsx
@@ -2,7 +2,7 @@ import { SectionHeadingProps } from "@/types";
 
 const SectionSmallHeading = (props: SectionHeadingProps) => {
   return (
-    <div className={`${props.className} text-lg text-center text-primary-500 font-bold mb-4 tracking-wider`}
+    <div className={`${props.className} text-lg text-center text-primary font-bold mb-4 tracking-wider`}
          data-test="heading-div"
     >
         <h3 data-test="text">{props.text}</h3>
