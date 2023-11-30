36f6173d7c87dd19ca052cbb00e1bdaf73f1f934
Owen Rees <owenrees82@gmail.com>
2023-11-30 19:51:56 +0100

    light grey neutral text

--- src/app/components/SectionDescription.tsx
+++ src/app/components/SectionDescription.tsx
@@ -2,7 +2,7 @@ import { SectionHeadingProps } from "@/types";
 
 const SectionDescription = (props: SectionHeadingProps) => {
   return (
-    <div className={`${props.className} mb-8 text-center text-gray-600 font-semibold w-1/2`}
+    <div className={`${props.className} mb-8 text-center text-neutral font-semibold w-1/2`}
          data-test="description-div"
     >
       <h5 data-test="text">{props.text}</h5>
