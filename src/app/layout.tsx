16e4fb584e2a4fdc913c9f35b9afb0b11d333d3b
Owen <owenrees82@gmail.com>
2024-10-02 11:42:25 +0200

    user cancelled lichess auth error and redirect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,47 +1,45 @@
-import { Analytics } from '@vercel/analytics/react'
 import { ReactNode } from "react";
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+
+import { Analytics } from "@vercel/analytics/react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import { ToastContainer } from "react-toastify";
-import './globals.css'
 import "react-toastify/dist/ReactToastify.css";
-import '@/scss/index.scss'
 
-import Navbar from '@/app/components/Navbar'
 import Footer from "@/app/components/Footer";
-
+import Navbar from "@/app/components/Navbar";
 import { Providers } from "@/providers/providers";
+import "@/scss/index.scss";
 
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'ChessScribe',
-  description: 'Create PDFs of your chess games from a PGN file',
-}
+  title: "ChessScribe",
+  description: "Create PDFs of your chess games from a PGN file",
+  keywords: ["chess", "pgn", "pdf", "chess games", "chess notation"],
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" data-theme="customLightTheme">
-    <head>
-      <link rel="icon" href="/favicon.svg" sizes="any"/>
-    </head>
-    <Providers>
-      <body className={inter.className}>
-        <div className="bg-base-100 grid">
-          <Navbar />
-            <div className="relative min-h-[calc(100vh-114px-80px)] bg-white place-self-center">
+      <head>
+        <link rel="icon" href="/favicon.svg" sizes="any" />
+      </head>
+      <Providers>
+        <body className={inter.className}>
+          <div className="grid bg-base-100">
+            <Navbar />
+            <div className="relative min-h-[calc(100vh-114px-80px)] place-self-center bg-white">
               {children}
               <ToastContainer position="bottom-right" />
             </div>
-          <Footer />
-        </div>
-        <Analytics />
-      </body>
-    </Providers>
+            <Footer />
+          </div>
+          <Analytics />
+        </body>
+      </Providers>
     </html>
-  )
+  );
 }
