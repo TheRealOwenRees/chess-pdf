e130df97e89f76f93414be5e45b3f6fc38664f93
Owen Rees <owenrees@tutanota.com>
2024-10-14 12:24:26 +0200

    Feature/import study link (#162)
    * Merge Development into Main (#150)
    
    * redirection change
    
    * redirection change
    
    * user cancelled lichess auth error and redirect
    
    * Merge Development into Main (#152)
    
    * dependencies updates
    
    * game load buttons spacing update
    
    * Update postcss to version 8.4.47
    
    * Development (#161)
    
    * project support
    
    * redirection change
    
    * study search and import link
    
    * form updates to lichess study url
    
    * loading state for import buttons
    
    * loading state for import buttons
    
    ---------
    
    Co-authored-by: depfu[bot] <23717796+depfu[bot]@users.noreply.github.com>

---
+++ src/hooks/useLichessStudy.ts
@@ -0,0 +1,123 @@
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+
+import { useAtom, useAtomValue } from "jotai/index";
+import { toast } from "react-toastify";
+
+import { gameAtom, lichessUserAtom } from "@/atoms";
+import useLichessOAuth from "@/hooks/useLichessOAuth";
+import usePgn from "@/hooks/usePgn";
+import { IChapter } from "@/types";
+import { getHeaders } from "@/utils/pgnUtils";
+
+interface IStudy {
+  id: string;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+const useLichessStudy = () => {
+  const lichessUser = useAtomValue(lichessUserAtom);
+  const [_, gameDispatch] = useAtom(gameAtom);
+  const [userStudies, setUserStudies] = useState<IStudy[] | undefined>([]);
+  const [studyChapters, setStudyChapters] = useState<IChapter[]>([]);
+  const [studySearchTerm, setStudySearchTerm] = useState("");
+  const [filteredUserStudies, setFilteredUserStudies] = useState<
+    IStudy[] | undefined
+  >([]);
+
+  const { clearPgn } = usePgn();
+  const { lichessGetUserStudies, lichessAllChapters } = useLichessOAuth();
+
+  // load user studies on login
+  useEffect(() => {
+    if (lichessUser.username && lichessUser.loggedIn) {
+      (async () => {
+        const studies = await lichessGetUserStudies();
+        setUserStudies(studies);
+        setFilteredUserStudies(studies);
+      })();
+    }
+  }, [lichessUser]);
+
+  const importPgn = (chapter: IChapter) => {
+    if (chapter.chapterId) {
+      const headers = getHeaders(chapter.pgn);
+      gameDispatch({
+        type: "SET_GAME",
+        payload: { pgn: chapter.pgn, headers: headers },
+      });
+      toast.success(`${chapter.name} imported successfully`, {
+        toastId: `${chapter.chapterId}-lichess-import-success`,
+      });
+    }
+  };
+
+  const studySearch = (e: ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = e.target.value;
+
+    if (!searchTerm) {
+      setStudySearchTerm("");
+      setFilteredUserStudies(userStudies);
+    }
+
+    setStudySearchTerm(searchTerm);
+    const filteredStudies = userStudies?.filter((study) =>
+      study.name.toLowerCase().includes(searchTerm.toLowerCase()),
+    );
+    setFilteredUserStudies(filteredStudies);
+  };
+
+  // clear search terms and reset filtered studies
+  const clearStudySearch = () => {
+    setStudySearchTerm("");
+    setFilteredUserStudies(userStudies);
+  };
+
+  // load chapters on study selection
+  const studySelection = async (studyId: string) => {
+    const response = await lichessAllChapters(studyId);
+
+    if ("error" in response) {
+      setStudyChapters([]);
+      toast.error(response.error);
+      return;
+    }
+
+    clearPgn();
+    setStudyChapters(response);
+    toast.success(`Study loaded`);
+  };
+
+  // parse study id from url or input, then load chapters
+  const importStudyFromUrl = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    let studyId: string;
+    const form = new FormData(e.currentTarget);
+    const input = form.get("lichessStudyUrl") as string;
+
+    if (input) {
+      studyId = input.includes("/") ? input.split("/").pop()! : input;
+      await studySelection(studyId);
+    }
+  };
+
+  return {
+    importPgn,
+    importStudyFromUrl,
+    userStudies,
+    setUserStudies,
+    filteredUserStudies,
+    setFilteredUserStudies,
+    studyChapters,
+    setStudyChapters,
+    studySearchTerm,
+    setStudySearchTerm,
+    clearStudySearch,
+    studySearch,
+    studySelection,
+  };
+};
+
+export default useLichessStudy;
